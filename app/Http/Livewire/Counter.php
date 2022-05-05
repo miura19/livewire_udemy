47c720618dbe8975682250e81978ef6482b71326
miura19 <littlecloud194@gmail.com>
2022-05-05 16:04:07 +0900

    ★wire:click()に引数渡せる★

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -7,10 +7,29 @@ use Livewire\Component;
 class Counter extends Component
 {
     public $counter = 0;
-    public function incriment()
+    //初期値は１、関数の引数が渡ってきたらそれが$numに入って足される
+    public function incriment(int $num = 1)
     {
-        $this->counter++;
+        if ($this->counter < 100)
+        {
+            $this->counter+= $num;
+        } else {
+            $this->counter = 1;
+        }
     }
+    // public function incriment10()
+    // {
+    //     if ($this->counter < 100)
+    //     {
+    //         $this->counter = $this->counter + 10;
+    //     } else {
+    //         $this->counter = 1;
+    //     }
+    // }
+    // public function incriment_reset()
+    // {
+    //     $this->counter = 0;
+    // }
     public function render()
     {
         return view('livewire.counter');
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,4 +1,6 @@
 <div>
     <p>現在のカウンター：{{ $counter }}</p>
     <input type="button" wire:click="incriment" value="+1">
+    <input type="button" wire:click="incriment(10)" value="+10">{{--関数に引数渡せる--}}
+    <input type="button" wire:click="$set('counter',0)" value="0にリセットする">{{--$set()こいつは特別な関数--}}
 </div>
