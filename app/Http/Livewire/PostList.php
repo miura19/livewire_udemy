ca4599ac28c1d302e340f8fd380c01c4b17a09b0
miura19 <littlecloud194@gmail.com>
2022-05-06 00:02:01 +0900

    ★Postモデルのタイトルをリストに全表示★

--- app/Http/Livewire/PostList.php
+++ app/Http/Livewire/PostList.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class PostList extends Component
 {
     public function render()
     {
-        return view('livewire.post-list');
+        return view('livewire.post-list',[
+            'posts' => Post::get(),
+        ]);
     }
 }
